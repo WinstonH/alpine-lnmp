bccd1cf984bd979956f2c5bd2de0a988c9600f87
Winston <bjdhjj@gmail.com>
2018-03-27 22:17:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@ sudo php-fpm7
 sudo nginx
 # 更新mysql配置
 mysql_install_db --user=mysql --datadir=${DB_DATA_PATH} 
-mysqld
+rc default
+rc-service mariadb start
 mysqladmin -u root password "${DB_ROOT_PASS}" \
 && echo "GRANT ALL ON *.* TO ${DB_USER}@'127.0.0.1' IDENTIFIED BY '${DB_PASS}' WITH GRANT OPTION;" > /tmp/sql \
 && echo "GRANT ALL ON *.* TO ${DB_USER}@'localhost' IDENTIFIED BY '${DB_PASS}' WITH GRANT OPTION;" >> /tmp/sql \
@@ -12,5 +13,6 @@ mysqladmin -u root password "${DB_ROOT_PASS}" \
 && echo "DROP DATABASE test;" >> /tmp/sql \
 && echo "FLUSH PRIVILEGES;" >> /tmp/sql \
 && cat /tmp/sql | mysql -u root --password="${DB_ROOT_PASS}"
+
 sudo /usr/bin/supervisord -c /etc/supervisord.conf
 exec "$@"
